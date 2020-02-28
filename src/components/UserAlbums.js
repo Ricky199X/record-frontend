c236007dd1b0662c54cc809861c67acda3b9fb13
github username <github email address>
2020-02-27 22:35:28 -0500

    sets up UserAlbums functional component

---
+++ src/components/UserAlbums.js
@@ -0,0 +1,15 @@
+// this component gets list of user albums - functional component
+
+import React from 'react'
+
+const UserAlbums = (props) => {
+
+   return (
+      <div>
+         UserAlbums
+      </div>
+   )
+}
+
+
+export default UserAlbums
