e23f9fe5803e5ad854722c2d4a3b4a670a670b4e
github username <github email address>
2020-02-27 22:20:35 -0500

    adds usersContainer

---
+++ src/containers/usersContainer.js
@@ -0,0 +1,18 @@
+// will render other components, pass them data if they require it, and contain the callback functions etc.
+
+import React from 'react'
+
+class usersContainer extends React.Component {
+
+
+   render() {
+      return (
+         <div>
+            Users Container
+         </div>
+      )
+   }
+
+}
+
+export default usersContainer
