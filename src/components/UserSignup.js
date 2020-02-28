f210d85b6097e25affa1c6f8760a4572dc5a18e8
github username <github email address>
2020-02-27 22:32:23 -0500

    adds user signup form

---
+++ src/components/UserSignup.js
@@ -0,0 +1,21 @@
+// this is a form component, could be used for both setting up account
+
+import React from 'react'
+
+class UserSignup extends React.Component {
+
+   render() {
+      return (
+         <div className="form">
+            {/* <form class="col s12">
+               <input type='text' name='username' placeholder='Enter your Username'/>
+               <input type='text' name='email' placeholder='Email address'/>
+               <input type='text' name='password' placeholder='Enter password' />
+            </form>  */}
+            UserSignup
+         </div>
+      )
+   }
+}
+
+export default UserSignup
