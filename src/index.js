98ed94a91d5b2fa19605011a0db52bb0a2bc9a07
github username <github email address>
2020-02-27 21:11:37 -0500

    sets up userReducer

--- src/index.js
+++ src/index.js
@@ -4,15 +4,14 @@ import App from './App';
 import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
+import userReducer from './reducers/userReducer'
 
-// // set up reducer
-// let reducer = ''
 
 // set up devtools
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 // set up store
-let store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)))
+let store = createStore(userReducer, composeEnhancers(applyMiddleware(thunk)))
 
 ReactDOM.render(
    <Provider store={store}> 
---
+++ src/reducers/userReducer.js
@@ -0,0 +1,8 @@
+// this will handle everything related to updating parts of the store that have to do with the user account
+
+// set the default state first, action is second argument
+export default function userReducer(state = {users: []}, action) {
+
+   return state
+
+}
