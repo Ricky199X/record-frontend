c750956e6c1e0488a8d768b2194922da055cbc24
github username <github email address>
2020-02-27 21:19:52 -0500

    sets up fecthUsers action creator

---
+++ src/actions/fetchUsers.js
@@ -0,0 +1,9 @@
+// this is the action creator that fetches user information 
+
+export function fetchUsers() {
+
+
+}
+
+// what we dispatch is an action object -> to the reducer
+// which will then return a new version of our state based on what action we sent
