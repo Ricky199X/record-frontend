aedb27956dccb2ce1de986e0c2ce728114cd2bc9
github username <github email address>
2020-02-27 20:55:56 -0500

    sets up store + devtools

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
 
+// // set up reducer
+// let reducer = ''
+
+// set up devtools
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 // set up store
+let store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)))
 
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+   <Provider store={store}> 
+      <App />
+   </Provider>
+, 
+document.getElementById('root'));
 
 
