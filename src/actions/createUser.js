4f8ae4f542378cab18272b114a290eccc6b048fe
github username <github email address>
2020-02-29 14:31:42 -0500

    sets up post request to DB  - prior to test

--- src/actions/createUser.js
+++ src/actions/createUser.js
@@ -1,9 +1,17 @@
 // this is the action creator that CREATES user information, sends it to DB
+// thunk is the reason why we have access to the dispatch function
 
 export const createUser = (data) => {
    // debugger
    return (dispatch) => {
-      
+      fetch('http://localhost:3000/users', {
+         headers: {
+            'Content-Type': 'application/json'
+            'Accept': 'application/json'
+         },
+         method: 'POST',
+         body: JSON.stringify(data)
+      })
    }
 
 
