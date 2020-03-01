628015bef11211d6096b46842f1ec9344c832bb6
github username <github email address>
2020-02-29 22:26:27 -0500

    adds landingPage component

---
+++ src/components/LandingPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class LandingPage extends React.Component {
+
+   render() {
+      return (
+         <h4>This is the landing page</h4>
+      )
+   }
+
+}
+
+
+export default LandingPage
