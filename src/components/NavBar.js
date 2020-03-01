e3df18c8d9e7b29e334229ca87073d02ed79c7a3
github username <github email address>
2020-02-29 23:46:22 -0500

    nav bar working

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 
 class NavBar extends React.Component {
 
@@ -9,15 +9,15 @@ class NavBar extends React.Component {
       return (
          <nav>
             <div className="nav-wrapper container">
-            <a className="brand-logo">Record Collector</a>
-               <ul className= "right">
-                  <li>Albums</li>
-                  <li>Your Collection</li>
-                  <li>Logout</li>
+               <a className="brand-logo">Record Collector</a>
+               <ul className = "right">
+                  <li><Link to="/">Home</Link></li>
+                  <li><Link to="/albums">Albums</Link></li>
+                  <li><Link to="/user_albums">Your Albums</Link></li>
                </ul>
             </div>
          </nav>
-      )
+      )  
    }
 }
 
