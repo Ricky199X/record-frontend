a3689650ccbdf4bb9b384d6ad407a24a36c569e8
github username <github email address>
2020-02-29 23:05:12 -0500

    setting up react routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux'
 import UsersContainer from './containers/UsersContainer';
+
+// components 
 import NavBar from './components/NavBar';
 import LandingPage from './components/LandingPage'
+import Albums from './components/Albums'
+import UserAlbums from './components/UserAlbums'
 
 
 class App extends React.Component {
---
+++ src/components/Albums.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+class Albums extends React.Component {
+
+
+
+   render() {
+      return (
+         <h4>This is the albums page</h4>
+      )
+   }
+} 
+
+export default Albums
