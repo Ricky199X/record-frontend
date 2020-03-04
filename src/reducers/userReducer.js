e36a900096eac563713b084003155429c81c30bc
github username <github email address>
2020-03-03 19:50:50 -0500

    adds loginUser action

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,16 +1,20 @@
 // this will handle everything related to updating parts of the store that have to do with the user account
 
 const initialState = {
-   currentUser: {}
+   data: {},
+   currentUser: {},
+   loading: false
 }
 
 // set the default state first, action is second argument
-export default function userReducer(state = {data: {}, loading: false}, action) {
+export default function userReducer(state = initialState, action) {
    switch (action.type) {
       case 'LOADING_USER':
          return {...state, loading: true}
       case 'ADD_USER':
          return {loading: false, data: action.payload}
+      case 'LOGIN_USER':
+         return {...state, currentUser: action.payload}
       default:
          return state
    }
