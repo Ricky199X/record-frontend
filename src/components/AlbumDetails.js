735802d071e0a361396256e011b57dfee539f126
github username <github email address>
2020-03-07 18:01:07 -0500

    adds album details component

---
+++ src/components/AlbumDetails.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class AlbumDetails extends React.Component {
+
+
+
+
+   render() {
+      <div>
+         <h1>These are the album details</h1> 
+      </div>
+   }
+}
+
+
+export default AlbumDetails
