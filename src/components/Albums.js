e918ebcf53bead8c6fbd9f3e694b2e2fec1ac63f
github username <github email address>
2020-03-18 16:45:02 -0400

    fixes weird album bug

--- src/components/Albums.js
+++ src/components/Albums.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import AlbumThumbnail from './AlbumThumbnail'
-import { BrowserRouter as Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 // will render all albums in db
 // when you click the album, it will render an album card
