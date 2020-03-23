989d22a26e200d0c6f6e67cb0bef4f6d7370233b
github username <github email address>
2020-03-23 07:42:48 -0400

    adds additional notes + organizes components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+// App.js is CONTAINER COMPONENT - Doesn't render any HTML of its own, strictly handles routing + manages app state
+
+// Redux and React-Router-Dom imports
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import { connect } from 'react-redux'
 import { getCurrentUser } from './actions/userActions'
 import { fetchAlbums } from './actions/albumActions'
 
-// Component Import
+// Component Imports
 import AlbumsContainer from './containers/AlbumsContainer'
 import NavBar from './components/NavBar';
 import Home from './components/Home'
@@ -41,8 +44,8 @@ class App extends React.Component {
             <Route exact path='/login' component={UserLogin}/>
             <Route exact path='/signup' component={UserSignup}/>
             <Route exact path='/dashboard' component={Dashboard} currentUserId={this.props.currentUserId} userLoginStatus={userLoginStatus} />
-            <Route exact path='/albums' component={AlbumsContainer} />
-            <Route path='/your-albums' component={Dashboard} currentUserId={this.props.currentUserId} />
+            <Route exact path='/albums' component={AlbumsContainer}/>
+            <Route path='/your-albums' component={Dashboard} currentUserId={this.props.currentUserId}/>
 
             <Route path='/albums/:id'>
               <AlbumDetails />
