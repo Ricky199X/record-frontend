d19755411a54accdea8f28e0228555a3f45a9b9c
github username <github email address>
2020-03-25 06:52:45 -0400

    cleans up code

--- src/components/UserAlbumDetails.js
+++ src/components/UserAlbumDetails.js
@@ -1,40 +1,25 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { getCurrentUser} from '../actions/userActions'
-import {withRouter } from 'react-router'
+import { withRouter } from 'react-router'
 
 
 class UserAlbumDetails extends React.Component {
 
-   // componentDidMount() {
-   //    this.props.getCurrentUser()  
-   // }
-
    renderUserAlbum = (userAlbums, albumId) => {
       const userAlbum = userAlbums.find(album => album.id === parseInt(albumId))
-      const currentUserId = this.props.user.data.id
 
       return (
          // need to handle case if null - need to re-render the previous page
          <div>
             <img alt={userAlbum.name} src={userAlbum.cover_url} width="300" height="300"/>
-            {/* <h5>Artist: {userAlbum.attributes.artist.name}</h5> */}
             <h5>Album Name: {userAlbum.name}</h5> 
             <h5>Release Date: {userAlbum.release_date}</h5> 
             <h5>Record Label: {userAlbum.label}</h5>
             <h5>Popularity: {userAlbum.popularity}</h5>
-   
-            {/* delete album button */}
-            {/* <button onClick={() => this.handleDeleteUserAlbum(albumId, currentUserId)}>Delete Album</button> */}
             
          </div>
       )
    }
-
-   // DELETE USER ALBUM ACTION - WILL NEED THIS LATER ON 
-   // handleDeleteUserAlbum = (albumId, currentUserId) => {
-   //    this.props.deleteUserAlbum(albumId, currentUserId)
-   // }
 
    render() {
       let userAlbums = this.props.user.user_albums
@@ -54,4 +39,4 @@ const mapStateToProps = ({user, albums}) => {
    return { user, albums } 
 }
  
-export default withRouter(connect(mapStateToProps, {getCurrentUser})(UserAlbumDetails))
+export default withRouter(connect(mapStateToProps)(UserAlbumDetails))
