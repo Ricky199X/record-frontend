6441be673b78d72e9fda1fef030c9d517981d66d
github username <github email address>
2020-03-25 22:28:25 -0400

    adds index.css + background image

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
 import rootReducer from './reducers'
+import './index.css'
 
 
 // set up Redux devtools
