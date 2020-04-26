93ecee8cd8222e2a32b52c222d1b1401ed690250
Ricky199X <rickyrojas19@gmail.com>
2020-04-26 18:46:05 -0400

    added landing page image to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,19 +3,35 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+
+// Import image
+import LandingPagePic from "..//assets/black-vinyl-player-145707 (2).jpg"
+
 const Home = () => {
    return (
-      <div class="homepage-links-container center">
+      <div class="homepage-container">
 
-         <button class="btn-large green accent-4">
-            <Link to='/signup' class="white-text">Sign Up</Link>
-         </button>
+         <section class="homepage-img-container center">
 
-         <button class="btn-large green accent-4">
-            <Link to='/login' class="white-text">Log In</Link>
-         </button>
+            <div class="img-div">
+               <img class="landing-page-pic" src={LandingPagePic} />
+            </div>
 
-      </div>
+
+         </section>
+
+
+         <section class="homepage-links-container center">
+            <button class="btn-large green accent-4">
+               <Link to='/signup' class="white-text">Sign Up</Link>
+            </button>
+
+            <button class="btn-large green accent-4">
+               <Link to='/login' class="white-text">Log In</Link>
+            </button>
+         </section>
+
+      </div >
    )
 }
 
